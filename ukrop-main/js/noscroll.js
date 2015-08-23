0d1ac7ea182a1ffed0cbcc6694dac407e63d8009
Katia Trifonova <trifonova.katia@yahoo.com>
2015-08-23 16:48:17 +0300

    noscroll.js edit

--- ukrop-main/js/noscroll.js
+++ ukrop-main/js/noscroll.js
@@ -1,20 +1,20 @@
 var map = document.querySelector(".js-map");
 var cover = document.querySelector(".js-map-cover");
-var addresses = document.querySelector(".js-addresses");
-var footer = document.querySelector(".js-footer");
+var prev = document.querySelector(".js-addresses");
+var next = document.querySelector(".js-footer");
 
 
 cover.addEventListener("click", function(e) {
   map.classList.remove("js-map-noscroll");
 });
 
-addresses.addEventListener("click", function(e) {
+prev.addEventListener("click", function(e) {
   if (!(map.classList.contains("js-map-noscroll"))) {
   map.classList.add("js-map-noscroll");
   }
 });
 
-footer.addEventListener("click", function(e) {
+next.addEventListener("click", function(e) {
   if (!(map.classList.contains("js-map-noscroll"))) {
   map.classList.add("js-map-noscroll");
   }
